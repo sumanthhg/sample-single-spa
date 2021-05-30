454b2b3b9913c1f7cd7b4a24ce2c872f0aefc94b
sumanthhg <sumanth.hgbhat@gmail.com>
2021-05-30 09:16:54 +0530

    navbar-app
    navbar-app

---
+++ navbar-app/src/root.component.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+//import { SampleButton } from "@sample-company/stylelib";
+export default function Root(props) {
+  return (
+    <>
+      <section>{props.name} is mounted!</section>
+      {/* <SampleButton>Sample Button example</SampleButton> */}
+    </>
+  );
+}
